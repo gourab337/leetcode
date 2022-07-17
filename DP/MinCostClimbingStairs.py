c084126b53ae2ed5e39c83404fd4716e0dacac19
gourab337 <19bcs118@iiitdwd.ac.in>
2022-07-18 00:21:32 +0530

    Added a few DP problems

---
+++ DP/MinCostClimbingStairs.py
@@ -0,0 +1,11 @@
+class Solution:
+    def minCostClimbingStairs(self, cost: List[int]) -> int:
+        
+        def dp(i):
+            if i<= 1:
+                return 0;
+            if i not in hashmap:
+                hashmap[i] = min(dp(i-2)+cost[i-2], dp(i-1)+cost[i-1])
+            return hashmap[i]
+        hashmap = {}
+        return dp(len(cost))
